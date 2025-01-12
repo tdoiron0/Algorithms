d8c251d29290022859cb590cea5726265c191a8e
Trent Doiron <103758611+tdoiron0@users.noreply.github.com>
2025-01-11 21:40:06 -0500

    natural log

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import Math.Matrix;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BigDecimal x = new BigDecimal("0.5");
+        BigDecimal x = new BigDecimal("1.5");
         BigDecimal y = Math.ln(x);
         System.out.println(y);
     }
